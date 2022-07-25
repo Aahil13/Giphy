019e50a2ad91462e890e3ace4fe87a57c5651eb4
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-07-25 20:48:10 +0100

    Added a gitignore fie

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
+require("dotenv").config();
+console.log(process.env)
+
 // for the date at the footer
 const date = new Date();
-const year  = date.getFullYear();
+const year = date.getFullYear();
 document.querySelector(".copyright-text").innerHTML = `&copy; Aahil ${year}`;
 let globalId;
 
 // getting and using the Api
 async function handleClick() {
-    try{
-        let inputValue = document.getElementById('search').value;
-        const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=deokzgUjxm6QHQdp3H3aca1LSZcCpucc&limit=1&q=${inputValue}`)
-        const data = await res.json()
-        console.log(data.data)
-        console.log(inputValue)
-        globalId = data.data[0].id
-        const gifContainer = document.querySelector('.gif-container');
-        gifContainer.innerHTML += `<div class="gif-layout"><a onclick="handleId()"><img src=${data.data[0].images.downsized.url} alt=${data.data[0].title}></a></div>`
-    }catch(err) {
-        console.error(err)
-    }
+  try {
+    let inputValue = document.getElementById("search").value;
+    const res = await fetch(process.env.API_URL);
+    const data = await res.json();
+    console.log(data.data);
+    console.log(inputValue);
+    globalId = data.data[0].id;
+    const gifContainer = document.querySelector(".gif-container");
+    gifContainer.innerHTML += `<div class="gif-layout"><a onclick="handleId()"><img src=${data.data[0].images.downsized.url} alt=${data.data[0].title}></a></div>`;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function handleId() {
-   window.location.href = `singleGif.html?id=${globalId}`
+  window.location.href = `singleGif.html?id=${globalId}`;
 }
-
-
-
- 
-
-
-
--- singleGif.js
+++ singleGif.js
@@ -1,24 +1,24 @@
 const date = new Date();
-const year  = date.getFullYear();
+const year = date.getFullYear();
 document.querySelector(".copyright-text").innerHTML = `&copy; Aahil ${year}`;
 
+let queryStr = window.location.search;
 
-let queryStr = window.location.search
+const urlParams = new URLSearchParams(queryStr);
 
-const urlParams = new URLSearchParams(queryStr)
+const id = urlParams.get("id");
 
-const id = urlParams.get('id')
+const urlId = decodeURIComponent(id).replace(/['"]+/g, "");
 
-const urlId = decodeURIComponent(id).replace(/['"]+/g, '')
-
-
-async function onLoad(){
-    try{
-        const res = await fetch(`https://api.giphy.com/v1/gifs/${urlId}?api_key=deokzgUjxm6QHQdp3H3aca1LSZcCpucc&q`);
-        const data = await res.json()
-        console.log(data)
-        const singleGifContainer = document.querySelector('.Single-gif-container');
-        singleGifContainer.innerHTML += `
+async function onLoad() {
+  try {
+    const res = await fetch(
+      `https://api.giphy.com/v1/gifs/${urlId}?api_key=deokzgUjxm6QHQdp3H3aca1LSZcCpucc&q`
+    );
+    const data = await res.json();
+    console.log(data);
+    const singleGifContainer = document.querySelector(".Single-gif-container");
+    singleGifContainer.innerHTML += `
                         <div>
                             <a onclick="handleId()">
                                 <img class='singleGif' src=${data.data.images.downsized.url} alt=${data.data.title}>
@@ -30,8 +30,8 @@ async function onLoad(){
                             <p>Rating :${data.data.rating}</p>
                             <p>Type :${data.data.type}</p>
                         </div>
-                        `
-    }catch(err) {
-        console.error(err)
-    }
+                        `;
+  } catch (err) {
+    console.error(err);
+  }
 }
